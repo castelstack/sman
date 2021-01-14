b598855600a022ff532059358052dbe761f7b4bb
castelstack <okaforhenry01@gmail.com>
2021-01-14 12:31:35 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route, useRouteMatch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from './containers/header/header';
 import Footer from './containers/footer/footer';
@@ -8,13 +8,12 @@ import Homepage from "../src/pages/homepage/homepage";
 import Gist from '../src/pages/gist/gist';
 import JoinUs from '../src/pages/join-us/join-us';
 
-function App() {
-  let { url } = useRouteMatch('/be-stingy');
+function App(props) {
+  
   return (
     <div className='App'>
-      <Router>{
-        (url === '/be-stingy') ? '' :
-        <Header /> }
+      <Router>
+        <Header /> 
         <Switch>
           <Route path='/' exact component={Homepage} />
           <Route path='/gist'  component={Gist} />
