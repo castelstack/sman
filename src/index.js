d90dca3a5ed83600d89fe8221a6ac1d0e7a33ba7
castelstack <okaforhenry01@gmail.com>
2021-02-02 01:36:55 +0100

    log out functoning

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {ActiveProvider} from './utils/store';
-import {AuthProvider} from './utils/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ActiveProvider } from "./utils/store";
+
 ReactDOM.render(
-  <AuthProvider>
-  <ActiveProvider >
-  <App />
-    </ActiveProvider>
-    </AuthProvider>
+  <ActiveProvider>
+    <App />
+  </ActiveProvider>,
 
-    ,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
